ae5937d3e5f0491dc16fb2ed8b487dc728802c64
Robin Voetter <robin@voetter.nl>
2021-04-03 15:06:59 +0200

    Fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+fish_add_path $HOME/.local/bin/
+
+function fish_user_key_bindings
+    bind \ch backward-kill-word
+end
+
+function config
+    git --git-dir=$HOME/.dotfiles --work-tree=$HOME $argv
+end
+
+function clip
+    xclip -selection c
+end
+
+config config status.showUntrackedFiles no
+tabs -4
