ac7508ac20f167c76b29a8da33f8b2d9af31b83c
Robin Voetter <robin@voetter.nl>
2021-06-24 15:28:17 +0200

    River

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,20 @@
 fish_add_path $HOME/.local/bin/
+bass source ~/.local/share/lunarg-vulkan-sdk/1.2.176.1/setup-env.sh
+
+set -x QT_QPA_PLATFORMTHEME qt5ct
+set -x QT_QPA_PLATFORM wayland
+set -x QT_WAYLAND_DISABLE_WINDOWDECORATION 1
+set -x SDL_VIDEODRIVER wayland
+set -x XDG_CURRENT_DESKTOP sway
+set -x XDG_SESSION_TYPE wayland
+set -x _JAVA_AWT_WM_NONREPARENTING 1
+set -x GDK_BACKEND wayland
+
+set gnome_schema org.gnome.desktop.interface
+gsettings set $gnome_schema gtk-theme Numix
+gsettings set $gnome_schema cursor-theme Breeze_Obsidian
+gsettings set $gnome_schema icon-theme Numix-Circle
+gsettings set $gnome_schema font-name "Cantarell 10"
 
 function fish_user_key_bindings
     bind \ch backward-kill-word
