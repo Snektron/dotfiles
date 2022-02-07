f295095975581fe5c054e38b527affc4733ccae2
Robin Voetter <robin@voetter.nl>
2022-02-07 09:21:33 +0100

    some misc stuff from laptop

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,13 +20,6 @@ gsettings set $gnome_schema cursor-theme Breeze_Obsidian
 gsettings set $gnome_schema icon-theme Numix-Circle
 gsettings set $gnome_schema font-name "Cantarell 10"
 
-if test -z (pgrep ssh-agent)
-    eval (ssh-agent -c) >/dev/null
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-end
-
 function fish_user_key_bindings
     bind \ch backward-kill-word
 end
